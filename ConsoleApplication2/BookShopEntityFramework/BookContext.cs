e6f1e88b57ace6212e4499654a839098e639f044
Софья Капчук <frostsofi@gmail.com>
2020-11-24 23:15:24 +0700

    Controller added

--- ConsoleApplication2/BookShopEntityFramework/BookContext.cs
+++ ConsoleApplication2/BookShopEntityFramework/BookContext.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using BookShopSystem;
+using System;
 
 namespace BookShopEntityFramework
 {
@@ -16,9 +17,39 @@ public class BookContext : DbContext
       modelBuilder.ApplyConfiguration(new BookConfiguration());
     }
 
+    public void AddBook(Book iBook)
+    {
+      Set<Book>().Add(iBook);
+    }
+
     public async Task<List<Book>> GetBooks()
     {
       return await Set<Book>().ToListAsync();
+    }
+
+    public Book GetBook(int iId)
+    {
+      return Set<Book>().Find(iId);
+    }
+
+    public void RemoveBook(int iId)
+    {
+      Book book = Set<Book>().FirstOrDefaultAsync(x => x.Id == iId).GetAwaiter().GetResult();
+
+      Set<Book>().Remove(book);
+    }
+
+    public bool UpdateBook(Book iBook)
+    {
+      if (iBook == null)
+      {
+        throw new ArgumentNullException("Book is null");
+      }
+      Book book = Set<Book>().FirstOrDefaultAsync(x => x.Id == iBook.Id).GetAwaiter().GetResult();
+
+      Set<Book>().Remove(book);
+      Set<Book>().Add(iBook);
+      return true;
     }
   }
 }
